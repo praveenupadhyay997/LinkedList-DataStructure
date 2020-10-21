daf4c319d8d423b50cca205eef0a48fc9d29335e
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-21 22:28:43 +0530

    [Praveen] Add: Creating a simple linked list with default functionality of addition at the end of node.

---
+++ LinkedListProblem/LinkedListProblem/Node.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Node.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Praveen Kumar Upadhyay"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace LinkedListProblem
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    /// <summary>
+    /// Class intended to initialise an instance of the node containing value and address of next node
+    /// </summary>
+    public class Node
+    {
+        public int value;
+        public Node next;
+        //PArameterised constuctor initialising the value of the node
+        public Node(int value)
+        {
+            this.value = value;
+        }
+    }
+}
